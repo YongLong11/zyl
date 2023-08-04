0c2686d58ba0bc67881b27d2fe54e11e77a7d88f
zhangyl31 <zhangyl31@ziroom.com>
2023-08-04 17:42:13 +0800

    feat: mybatis generator

---
+++ src/main/java/com/ziroom/zyl/mybatisGenerate/dao/entity/ZylUser.java
@@ -0,0 +1,63 @@
+package com.ziroom.zyl.mybatisGenerate.dao.entity;
+
+public class ZylUser {
+    private Integer id;
+
+    private String userName;
+
+    private String age;
+
+    private String code;
+
+    private String email;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
